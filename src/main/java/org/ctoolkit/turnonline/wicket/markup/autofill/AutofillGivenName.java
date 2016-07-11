30ace3133e462b5f3126a5411e957f186862b324
Aurel Medvegy <aurel.medvegy@ctoolkit.org>
2016-07-11 23:18:40 +0200

    The postal address panel has been implemented incl. related autofill behaviors.

---
+++ src/main/java/org/ctoolkit/turnonline/wicket/markup/autofill/AutofillGivenName.java
@@ -0,0 +1,24 @@
+package org.ctoolkit.turnonline.wicket.markup.autofill;
+
+/**
+ * The autofill {@link Autofill#GIVEN_NAME} singleton behavior implementation.
+ *
+ * @author <a href="mailto:aurel.medvegy@ctoolkit.org">Aurel Medvegy</a>
+ */
+public class AutofillGivenName
+        extends AutofillBehavior
+{
+    private static final long serialVersionUID = 6963501782850761047L;
+
+    private static AutofillGivenName INSTANCE = new AutofillGivenName();
+
+    private AutofillGivenName()
+    {
+        super( Autofill.GIVEN_NAME );
+    }
+
+    public static AutofillGivenName get()
+    {
+        return INSTANCE;
+    }
+}
---
+++ src/main/java/org/ctoolkit/turnonline/wicket/markup/autofill/AutofillOrganization.java
@@ -0,0 +1,24 @@
+package org.ctoolkit.turnonline.wicket.markup.autofill;
+
+/**
+ * The autofill {@link Autofill#ORGANIZATION} singleton behavior implementation.
+ *
+ * @author <a href="mailto:aurel.medvegy@ctoolkit.org">Aurel Medvegy</a>
+ */
+public class AutofillOrganization
+        extends AutofillBehavior
+{
+    private static final long serialVersionUID = -7278221563548907328L;
+
+    private static AutofillOrganization INSTANCE = new AutofillOrganization();
+
+    private AutofillOrganization()
+    {
+        super( Autofill.ORGANIZATION );
+    }
+
+    public static AutofillOrganization get()
+    {
+        return INSTANCE;
+    }
+}
